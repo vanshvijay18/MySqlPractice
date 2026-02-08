f4f647253ca8c13198eba088987f5bcaeb21619d
Vansh Vijayvargiya <146448479+vanshvijay18@users.noreply.github.com>
2026-02-08 18:55:23 +0530

    Create NameOfCityWherePopulLarge.sql

---
+++ NameOfCityWherePopulLarge.sql
@@ -0,0 +1,6 @@
+-- Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
+--The CITY table is described as follows:
+
+SELECT NAME
+FROM CITY
+WHERE CountryCode = 'USA'AND Population > 120000;
