0459926a3b277a9970dee8ed762b8071a1b9b30a
Vansh Vijayvargiya <146448479+vanshvijay18@users.noreply.github.com>
2026-02-11 23:05:35 +0530

    Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
    Add SQL query to select distinct city names ending with vowels.

---
+++ WeatherObservationStation7.sql
@@ -0,0 +1,3 @@
+-- Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+
+select distinct(city) from station where lower(right(city,1)) in('a','e','i','o','u');
